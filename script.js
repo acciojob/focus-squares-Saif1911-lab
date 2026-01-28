d88faf4581d853357a5d39b2899a14c0ccd0045f
Mohammed Saif <mohdsaifwork19@gmail.com>
2026-01-28 16:59:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((currentSquare) => {
+  currentSquare.addEventListener("mouseenter", () => {
+    squares.forEach((sq) => {
+      if (sq !== currentSquare) {
+        sq.style.backgroundColor = "#6F4E37"; // Coffee
+      }
+    });
+  });
+
+  currentSquare.addEventListener("mouseleave", () => {
+    squares.forEach((sq) => {
+      sq.style.backgroundColor = "#E6E6FA"; // Lavender
+    });
+  });
+});
